0bfa2d5bf85783b1813edc2cd240cf2c864dfca9
Jakub Jurysek <jakubjurysek@astronomie,cz>
2024-03-22 13:48:05 +0100

    function api first try

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,43 @@ extensions = [
 numpydoc_show_class_members = False
 autosummary_generate = True
 
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.8", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy", None),
+    "astropy": ("https://docs.astropy.org/en/latest/", None),
+    "pytables": ("https://www.pytables.org/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "matplotlib": ("https://matplotlib.org/", None),
+    "traitlets": ("https://traitlets.readthedocs.io/en/stable/", None),
+    "ctapipe": ("https://ctapipe.readthedocs.io/en/v0.17.1/", None)
+}
+
+# These links are ignored in the checks, necessary due to broken intersphinx for
+# these
+nitpick_ignore = [
+    ("py:class", "cts_core.camera.Camera"),
+    ("py:class", "ctapipe.instrument.camera.geometry.CameraGeometry"),
+    ("py:class", "ctapipe.core.tool.Tool"),
+    ("py:class", "ctapipe.core.component.Component"),
+    ("py:class", "ctapipe.core.container.Container"),
+    ("py:class", "ctapipe.calib.camera.flatfield.FlatFieldCalculator"),
+    ("py:class", "ctapipe.calib.camera.pedestals.PedestalCalculator"),
+    # coming from inherited traitlets docs
+    ("py:class", "t.Union"),
+    ("py:class", "t.Dict"),
+    ("py:class", "t.Tuple"),
+    ("py:class", "t.List"),
+    ("py:class", "t.Any"),
+    ("py:class", "t.Type"),
+    ("py:class", "Config"),
+    ("py:class", "Unicode"),
+    ("py:class", "StrDict"),
+    ("py:class", "ClassesType")
+]
+
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
