cb55176d099142ae6f670750eee7da24531bf37a
calispac <23462345+calispac@users.noreply.github.com>
2026-06-01 10:32:50 +0200

    add test to import packages

--- tests/test_import.py
+++ tests/test_import.py
@@ -15,3 +15,7 @@ def test_import_ctapipe():
 
     module, version = get_module_and_version("ctapipe")
     assert version is not None
+
+def test_import_protozfits():
+    module, version = get_module_and_version("protozfits")
+    assert version is not None
