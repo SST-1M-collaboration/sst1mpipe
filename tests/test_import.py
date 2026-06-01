f90ce871196d91f06098767647326c75cb2e6f9f
calispac <23462345+calispac@users.noreply.github.com>
2026-06-01 11:32:10 +0200

    remove duplicate depedencies

--- tests/test_import.py
+++ tests/test_import.py
@@ -17,7 +17,13 @@ def test_import_ctapipe():
     module, version = get_module_and_version("ctapipe")
     assert version is not None
 
+def test_import_gammapy():
+
+    module, version = get_module_and_version("gammapy")
+    assert version is not None
 
 def test_import_protozfits():
     module, version = get_module_and_version("protozfits")
     assert version is not None
+
+
