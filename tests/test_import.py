061a15ba5cb71efe53a45684877762075d8f349d
calispac <23462345+calispac@users.noreply.github.com>
2026-06-01 10:57:30 +0200

    use f-string and newline at en of file (fix W292 + UP032)

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,17 +4,19 @@ def get_module_and_version(pkg):
 
     module = importlib.import_module(pkg)
     version = getattr(module, "__version__", None)
-    print("Module {}, version {}".format(pkg, version))
+    print(f"Module {pkg}, version {version}")
     return module, version
 
 def test_import_astropy():
     module, version = get_module_and_version("astropy")
     assert version is not None
 
+
 def test_import_ctapipe():
 
     module, version = get_module_and_version("ctapipe")
     assert version is not None
+
 
 def test_import_protozfits():
     module, version = get_module_and_version("protozfits")
