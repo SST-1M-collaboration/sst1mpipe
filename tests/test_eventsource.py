75de82995769e4b038f413dd1238b9e6af05fcd6
calispac <23462345+calispac@users.noreply.github.com>
2026-06-09 13:56:37 +0200

    syntax fix

--- tests/test_eventsource.py
+++ tests/test_eventsource.py
@@ -3,8 +3,8 @@ from importlib.resources import files
 
 from sst1mpipe.io.sst1m_event_source import SST1MEventSource
 
-FILE_TEL_1 = str(files('sst1mpipe.resources.zfits').joinpath('SST1M1_20260121_0001.fits.fz'))
-FILE_TEL_2 = str(files('sst1mpipe.resources.zfits').joinpath('SST1M2_20260121_0001.fits.fz'))
+FILE_TEL_1 = files('sst1mpipe.resources.zfits').joinpath('SST1M1_20260121_0001.fits.fz')
+FILE_TEL_2 = files('sst1mpipe.resources.zfits').joinpath('SST1M2_20260121_0001.fits.fz')
 
 MAX_ITERATIONS = 10
 
@@ -15,7 +15,7 @@ def test_test_tiles_exists():
 
 def test_read_events():
 
-    source = SST1MEventSource([FILE_TEL_1], max_events=MAX_ITERATIONS)
+    source = SST1MEventSource([str(FILE_TEL_1)], max_events=MAX_ITERATIONS)
 
     i = 0
     for _ in source:
