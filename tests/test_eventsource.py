a267e913a7841793dbbaea92f66466b4452eacba
calispac <23462345+calispac@users.noreply.github.com>
2026-06-10 14:18:51 +0200

    add check on event types

--- tests/test_eventsource.py
+++ tests/test_eventsource.py
@@ -2,6 +2,7 @@ import os.path
 from importlib.resources import files
 
 from sst1mpipe.io.sst1m_event_source import SST1MEventSource
+from sst1mpipe.io.containers import CameraEventType
 
 FILE_TEL_1 = files('sst1mpipe.resources.zfits').joinpath('SST1M1_20260121_0001.fits.fz')
 FILE_TEL_2 = files('sst1mpipe.resources.zfits').joinpath('SST1M2_20260121_0001.fits.fz')
@@ -15,6 +16,7 @@ SUM_WAVEFORM_1 = [22699322, 22695204, 22699302, 22700002, 22699551]
 SUM_BASELINE_1 = [453938.875, 453933.25, 453942.0625, 453939.875, 453923.375]
 LOCAL_CAMERA_CLOCK_1 = [1769015262383583536, 1769015262384583536, 1769015262385583536,
                         1769015262386583536, 1769015262387583536, ]
+CAMERA_EVENT_TYPE_1 = [CameraEventType.INTERNAL, CameraEventType.INTERNAL, CameraEventType.INTERNAL , CameraEventType.INTERNAL , CameraEventType.INTERNAL]
 
 TEL_2_ID = 22
 
@@ -38,5 +40,6 @@ def test_read_events():
         assert baseline.sum() == SUM_BASELINE_1[i]
         assert event.sst1m.r0.tel[TEL_1_ID].gps_time == 0
         assert event.sst1m.r0.tel[TEL_1_ID].local_camera_clock == LOCAL_CAMERA_CLOCK_1[i]
+        assert event.sst1m.r0.tel[TEL_1_ID].camera_event_type == CAMERA_EVENT_TYPE_1[i]
         i += 1
     assert i == MAX_ITERATIONS
