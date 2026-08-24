17ea712ace123133f670bf493c6d54a47cbb56e3
calispac <23462345+calispac@users.noreply.github.com>
2026-08-24 13:48:35 +0200

    use ctapipe=0.18.0 to be able to get inherited classes of Component to be configurable

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -1,7 +1,9 @@
 import numpy as np
+from ctapipe.image import ImageProcessor
 from scipy.sparse import csr_matrix
 
 from ctapipe.image.cleaning import apply_time_delta_cleaning, ImageCleaner
+from traitlets.config import Config
 
 from sst1mpipe.instrument.camera import Camera
 from sst1mpipe.io.sst1m_event_source import SST1MEventSource
@@ -35,3 +37,20 @@ def test_load_DBSCANCleaning_from_name():
     cleaner = ImageCleaner.from_name(subarray=SUBARRAY, name='DBSCANImageCleaner3D')
     assert isinstance(cleaner, DBSCANImageCleaner3D)
 
+def test_DBSCAN_configurable_from_image_processor():
+
+    config = Config({"ImageProcessor": {
+        "image_cleaner_type": "DBSCANImageCleaner",
+        "use_telescope_frame": False,
+        "DBSCANImageCleaner": {
+            "epsilon_r": [
+                ["id", 1, 38],
+                ["id", 2, 38]
+            ],
+            "minimum_pe": [
+                ["id", 1, 30],
+                ["id", 2, 30]
+            ]
+        }}})
+
+    ImageProcessor(subarray=SUBARRAY, config=config)
