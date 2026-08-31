4a9f9c1d34a6bc490c103adce6536edf903697b0
calispac <23462345+calispac@users.noreply.github.com>
2026-08-31 13:21:36 +0200

    ruff format and fix

--- sst1mpipe/utils/cleaning.py
+++ sst1mpipe/utils/cleaning.py
@@ -9,10 +9,7 @@ from ctapipe.containers import (
     ImageParametersContainer,
 )
 
-from ctapipe.core.traits import (
-    FloatTelescopeParameter,
-    IntTelescopeParameter
-)
+from ctapipe.core.traits import FloatTelescopeParameter, IntTelescopeParameter
 
 from ctapipe.image import (
     ImageCleaner,
@@ -24,8 +21,8 @@ from ctapipe.image import (
 from scipy.spatial.distance import cdist
 from scipy.sparse import issparse
 
-from sklearn.cluster import DBSCAN
-from sklearn.neighbors import radius_neighbors_graph, sort_graph_by_row_values
+from sklearn.neighbors import radius_neighbors_graph
+
 
 def get_only_main_island_mask(geom, cleaning_mask):
 
@@ -41,10 +38,13 @@ def get_only_main_island_mask(geom, cleaning_mask):
         new_image_mask = cleaning_mask
     return new_image_mask
 
+
 def clean_dbscan_fast(sparse_connectivity_matrix, weights, min_points):
 
     if not issparse(sparse_connectivity_matrix):
-        RuntimeWarning("Connectivity matrix is not sparse it might reduce computation performances")
+        RuntimeWarning(
+            "Connectivity matrix is not sparse it might reduce computation performances"
+        )
 
     n_points = sparse_connectivity_matrix.dot(weights)
     mask_core = n_points >= min_points
@@ -75,25 +75,28 @@ class ImageCleanerSST(ImageCleaner):
         Apply SST-1M dynamic cleaning. See `ImageCleaner.__call__()`
         """
 
-        defaults = self.config['telescope_defaults']['tel_0'+str(tel_id)]
+        defaults = self.config["telescope_defaults"]["tel_0" + str(tel_id)]
 
         for setting in defaults:
-            min_nsb_level = setting['min_nsb_level']
-            stdev_scaling = setting['stdev_scaling']
-            picture_threshold_pe = setting['picture_threshold_pe']
-            boundary_threshold_pe = setting['boundary_threshold_pe']
-            min_picture_neighbors = setting['min_picture_neighbors']
-            keep_isolated = setting['keep_isolated']
-            only_main_island = setting['only_main_island']
-            min_time_neighbors = setting['min_time_neighbors']
-            time_limit_ns = setting['time_limit_ns']
+            min_nsb_level = setting["min_nsb_level"]
+            stdev_scaling = setting["stdev_scaling"]
+            picture_threshold_pe = setting["picture_threshold_pe"]
+            boundary_threshold_pe = setting["boundary_threshold_pe"]
+            min_picture_neighbors = setting["min_picture_neighbors"]
+            keep_isolated = setting["keep_isolated"]
+            only_main_island = setting["only_main_island"]
+            min_time_neighbors = setting["min_time_neighbors"]
+            time_limit_ns = setting["time_limit_ns"]
             if self.nsb_level >= min_nsb_level:
                 break
 
-        pic_thr=np.maximum(picture_threshold_pe, self.average_charge + stdev_scaling*self.stdev_charge)
+        pic_thr = np.maximum(
+            picture_threshold_pe,
+            self.average_charge + stdev_scaling * self.stdev_charge,
+        )
         geom = self.subarray.tel[tel_id].camera.geometry
         try:
-            self.frac_rised = sum(pic_thr > picture_threshold_pe)/float(len(pic_thr))
+            self.frac_rised = sum(pic_thr > picture_threshold_pe) / float(len(pic_thr))
             self.raised += pic_thr > picture_threshold_pe
         except Exception:
             self.frac_rised = 0.0
@@ -101,18 +104,18 @@ class ImageCleanerSST(ImageCleaner):
         mask_tailcuts = tailcuts_clean(
             geom,
             image,
-            picture_thresh = pic_thr,
-            boundary_thresh = boundary_threshold_pe,
-            min_number_picture_neighbors = min_picture_neighbors,
-            keep_isolated_pixels = keep_isolated,
+            picture_thresh=pic_thr,
+            boundary_thresh=boundary_threshold_pe,
+            min_number_picture_neighbors=min_picture_neighbors,
+            keep_isolated_pixels=keep_isolated,
         )
 
         time_delta_cleaning_mask = apply_time_delta_cleaning(
             geom,
-            mask = mask_tailcuts,
-            arrival_times = arrival_times,
-            min_number_neighbors = min_time_neighbors,
-            time_limit = time_limit_ns
+            mask=mask_tailcuts,
+            arrival_times=arrival_times,
+            min_number_neighbors=min_time_neighbors,
+            time_limit=time_limit_ns,
         )
 
         if only_main_island:
@@ -121,8 +124,8 @@ class ImageCleanerSST(ImageCleaner):
 
     # for testing, might disappear in the future
     def dump(self):
-        for ii,r in enumerate(self.raised):
-            logging.info("pixel %i raised %i times",ii,r)
+        for ii, r in enumerate(self.raised):
+            logging.info("pixel %i raised %i times", ii, r)
 
 
 class ImageCleanerSST_MC(ImageCleaner):
@@ -142,17 +145,17 @@ class ImageCleanerSST_MC(ImageCleaner):
         Apply SST-1M dynamic cleaning. See `ImageCleaner.__call__()`
         """
 
-        defaults = self.config['telescope_defaults']['tel_00'+str(tel_id)]
+        defaults = self.config["telescope_defaults"]["tel_00" + str(tel_id)]
 
         for setting in defaults:
-            min_nsb_level = setting['min_nsb_level']
-            picture_threshold_pe = setting['picture_threshold_pe']
-            boundary_threshold_pe = setting['boundary_threshold_pe']
-            min_picture_neighbors = setting['min_picture_neighbors']
-            keep_isolated = setting['keep_isolated']
-            only_main_island = setting['only_main_island']
-            min_time_neighbors = setting['min_time_neighbors']
-            time_limit_ns = setting['time_limit_ns']
+            min_nsb_level = setting["min_nsb_level"]
+            picture_threshold_pe = setting["picture_threshold_pe"]
+            boundary_threshold_pe = setting["boundary_threshold_pe"]
+            min_picture_neighbors = setting["min_picture_neighbors"]
+            keep_isolated = setting["keep_isolated"]
+            only_main_island = setting["only_main_island"]
+            min_time_neighbors = setting["min_time_neighbors"]
+            time_limit_ns = setting["time_limit_ns"]
             if self.nsb_level >= min_nsb_level:
                 break
 
@@ -161,18 +164,18 @@ class ImageCleanerSST_MC(ImageCleaner):
         mask_tailcuts = tailcuts_clean(
             geom,
             image,
-            picture_thresh = picture_threshold_pe,
-            boundary_thresh = boundary_threshold_pe,
-            min_number_picture_neighbors = min_picture_neighbors,
-            keep_isolated_pixels = keep_isolated,
+            picture_thresh=picture_threshold_pe,
+            boundary_thresh=boundary_threshold_pe,
+            min_number_picture_neighbors=min_picture_neighbors,
+            keep_isolated_pixels=keep_isolated,
         )
 
         time_delta_cleaning_mask = apply_time_delta_cleaning(
             geom,
-            mask = mask_tailcuts,
-            arrival_times = arrival_times,
-            min_number_neighbors = min_time_neighbors,
-            time_limit = time_limit_ns
+            mask=mask_tailcuts,
+            arrival_times=arrival_times,
+            min_number_neighbors=min_time_neighbors,
+            time_limit=time_limit_ns,
         )
 
         if only_main_island:
@@ -184,13 +187,15 @@ class DBSCANImageCleaner(ImageCleaner):
     """
     An image cleaner based on the sklearn.cluster.DBSCAN algorithm
     """
+
     minimum_pe = IntTelescopeParameter(
         default_value=30, help="Minimum number of p.e. in cluster"
     ).tag(config=True)
 
-    picture_threshold_pe = FloatTelescopeParameter(default_value=0.0,
-                                                help="Minimum number of p.e. in the image the pixel.",
-                                                   ).tag(config=True)
+    picture_threshold_pe = FloatTelescopeParameter(
+        default_value=0.0,
+        help="Minimum number of p.e. in the image the pixel.",
+    ).tag(config=True)
 
     epsilon_r = FloatTelescopeParameter(
         default_value=38.0, help="Scale parameter for spatial coordinates (in mm)"
@@ -201,13 +206,22 @@ class DBSCANImageCleaner(ImageCleaner):
 
         self._precompute_distances()
 
-    def __call__(self, tel_id: int, image: np.ndarray, arrival_times: np.ndarray = None) -> np.ndarray:
+    def __call__(
+        self, tel_id: int, image: np.ndarray, arrival_times: np.ndarray = None
+    ) -> np.ndarray:
 
-        mask = clean_dbscan_fast(self._distances[tel_id], weights=image, min_points=self.minimum_pe.tel[tel_id])
-        mask = mask & (image > self.picture_threshold_pe.tel[tel_id]) # negative value are not accepted for Hillas computation
+        mask = clean_dbscan_fast(
+            self._distances[tel_id],
+            weights=image,
+            min_points=self.minimum_pe.tel[tel_id],
+        )
+        mask = mask & (
+            image > self.picture_threshold_pe.tel[tel_id]
+        )  # negative value are not accepted for Hillas computation
 
-        if mask.sum() <= 1: # Cleaning with one pixel fails the timing computation (impossible with 0)
-
+        if (
+            mask.sum() <= 1
+        ):  # Cleaning with one pixel fails the timing computation (impossible with 0)
             mask[...] = False
 
         return mask
@@ -216,9 +230,8 @@ class DBSCANImageCleaner(ImageCleaner):
 
         self._distances = {}
         for tel_id in self.subarray.tel_ids:
-
             geometry = self.subarray.tel[tel_id].camera.geometry
-            epsilon_r =self.epsilon_r.tel[tel_id] * u.mm
+            epsilon_r = self.epsilon_r.tel[tel_id] * u.mm
             x = np.column_stack([geometry.pix_x, geometry.pix_y]) / epsilon_r
 
             d = radius_neighbors_graph(
@@ -226,22 +239,25 @@ class DBSCANImageCleaner(ImageCleaner):
                 radius=1.0,
                 mode="connectivity",
                 include_self=True,
-                n_jobs=-1
+                n_jobs=-1,
             )
 
             self._distances[tel_id] = d
 
+
 class TimeDBSCANImageCleaner(ImageCleaner):
     """
-       An image cleaner based on the sklearn.cluster.DBSCAN algorithm that uses the image and peak time image
-       """
+    An image cleaner based on the sklearn.cluster.DBSCAN algorithm that uses the image and peak time image
+    """
+
     minimum_pe = IntTelescopeParameter(
         default_value=30, help="Minimum number of p.e. in cluster"
     ).tag(config=True)
 
-    picture_threshold_pe = FloatTelescopeParameter(default_value=0.0,
-                                                   help="Minimum number of p.e. in the image the pixel.",
-                                                   ).tag(config=True)
+    picture_threshold_pe = FloatTelescopeParameter(
+        default_value=0.0,
+        help="Minimum number of p.e. in the image the pixel.",
+    ).tag(config=True)
 
     epsilon_r = FloatTelescopeParameter(
         default_value=38.0, help="Scale parameter for spatial coordinates (in mm)"
@@ -256,17 +272,22 @@ class TimeDBSCANImageCleaner(ImageCleaner):
 
         self._precompute_distances_squared()
 
-    def __call__(self, tel_id: int, image: np.ndarray, arrival_times: np.ndarray) -> np.ndarray:
+    def __call__(
+        self, tel_id: int, image: np.ndarray, arrival_times: np.ndarray
+    ) -> np.ndarray:
 
         times = arrival_times / self.epsilon_t.tel[tel_id]
-        d = (times[:, None] - times[None, :])**2 + self._distances_squared[tel_id]
+        d = (times[:, None] - times[None, :]) ** 2 + self._distances_squared[tel_id]
         d = np.sqrt(d) <= 1.0
 
-        mask = clean_dbscan_fast(d, weights=image, min_points=self.minimum_pe.tel[tel_id])
+        mask = clean_dbscan_fast(
+            d, weights=image, min_points=self.minimum_pe.tel[tel_id]
+        )
         mask = mask & (image > self.picture_threshold_pe.tel[tel_id])
 
-        if mask.sum() <= 1: # Cleaning with one pixel fails the timing computation (impossible with 0)
-
+        if (
+            mask.sum() <= 1
+        ):  # Cleaning with one pixel fails the timing computation (impossible with 0)
             mask[...] = False
 
         return mask
@@ -275,7 +296,6 @@ class TimeDBSCANImageCleaner(ImageCleaner):
         self._distances_squared = {}
 
         for tel_id in self.subarray.tel_ids:
-
             geometry = self.subarray.tel[tel_id].camera.geometry
             x = np.column_stack([geometry.pix_x, geometry.pix_y])
             d = cdist(x.value, x.value) * x.unit
@@ -283,10 +303,12 @@ class TimeDBSCANImageCleaner(ImageCleaner):
 
             self._distances_squared[tel_id] = d**2
 
+
 class DBSCANImageCleaner3D(ImageCleaner):
     """
-       An image cleaner based on the sklearn.cluster.DBSCAN algorithm that uses the waveforms
-       """
+    An image cleaner based on the sklearn.cluster.DBSCAN algorithm that uses the waveforms
+    """
+
     minimum_pe = IntTelescopeParameter(
         default_value=30, help="Minimum number of p.e. in cluster"
     ).tag(config=True)
@@ -308,22 +330,31 @@ class DBSCANImageCleaner3D(ImageCleaner):
 
         self._precompute_distances()
 
-    def __call__(self, tel_id: int, waveform: np.ndarray, arrival_times = None) -> np.ndarray:
+    def __call__(
+        self, tel_id: int, waveform: np.ndarray, arrival_times=None
+    ) -> np.ndarray:
 
-        # clustering = DBSCAN(eps=1.0, min_samples=self.minimum_pe.tel[tel_id], metric='precomputed', n_jobs=-1)
-        # clustering.fit(self._distances[tel_id], sample_weight=waveform.ravel())
-        #
-        # mask = clustering.labels_ >= 0
+        mask = clean_dbscan_fast(
+            self._distances[tel_id],
+            weights=waveform.ravel(),
+            min_points=self.minimum_pe.tel[tel_id],
+        )
 
-        mask = clean_dbscan_fast(self._distances[tel_id], weights=waveform.ravel(), min_points=self.minimum_pe.tel[tel_id])
-
-        mask = mask.reshape((self.subarray.tel[tel_id].camera.readout.n_pixels, self.subarray.tel[tel_id].camera.readout.n_samples))
+        mask = mask.reshape(
+            (
+                self.subarray.tel[tel_id].camera.readout.n_pixels,
+                self.subarray.tel[tel_id].camera.readout.n_samples,
+            )
+        )
         mask = mask.sum(axis=-1)
         mask = mask >= self.min_samples.tel[tel_id]
 
-        if mask.sum() <= 1: # Cleaning with one pixel fails the timing computation (impossible with 0)
-
-            return np.zeros(self.subarray.tel[tel_id].camera.readout.n_pixels, dtype=bool)
+        if (
+            mask.sum() <= 1
+        ):  # Cleaning with one pixel fails the timing computation (impossible with 0)
+            return np.zeros(
+                self.subarray.tel[tel_id].camera.readout.n_pixels, dtype=bool
+            )
 
         return mask
 
@@ -332,35 +363,46 @@ class DBSCANImageCleaner3D(ImageCleaner):
         self._distances = {}
 
         for tel_id in self.subarray.tel_ids:
-
             geometry = self.subarray.tel[tel_id].camera.geometry
             readout = self.subarray.tel[tel_id].camera.readout
 
             t = np.arange(readout.n_samples) / readout.sampling_rate
 
+            indices_xyt = np.column_stack(
+                [
+                    np.repeat(
+                        geometry.pix_x.to(u.mm).value / self.epsilon_r.tel[tel_id],
+                        readout.n_samples,
+                    ),
+                    np.repeat(
+                        geometry.pix_y.to(u.mm).value / self.epsilon_r.tel[tel_id],
+                        readout.n_samples,
+                    ),
+                    np.tile(
+                        t.to(u.ns).value / self.epsilon_t.tel[tel_id], geometry.n_pixels
+                    ),
+                ]
+            )
 
-            indices_xyt = np.column_stack([
-                np.repeat(geometry.pix_x.to(u.mm).value / self.epsilon_r.tel[tel_id], readout.n_samples),
-                np.repeat(geometry.pix_y.to(u.mm).value / self.epsilon_r.tel[tel_id], readout.n_samples),
-                np.tile(t.to(u.ns).value / self.epsilon_t.tel[tel_id], geometry.n_pixels)
-            ])
-
-
-            d = radius_neighbors_graph(indices_xyt, 1.0, mode="connectivity",
-                                           include_self=True)
+            d = radius_neighbors_graph(
+                indices_xyt, 1.0, mode="connectivity", include_self=True
+            )
 
             self._distances[tel_id] = d
 
+
 def image_cleaner_setup(subarray=None, config=None, ismc=False):
 
-    cleaner = config['ImageProcessor']['image_cleaner_type']
+    cleaner = config["ImageProcessor"]["image_cleaner_type"]
 
     # If we use other than a standard cleaner recognized by ctapipe
     # (['TailcutsImageCleaner', 'MARSImageCleaner', 'FACTImageCleaner', 'TimeConstrainedImageCleaner'])
     # we have to setup its configuration manualy. Is there a better way to tackle this?
-    if cleaner == 'ImageCleanerSST':
-        image_processor   = ImageProcessor(subarray=subarray)
-        image_processor.use_telescope_frame = config['ImageProcessor']['use_telescope_frame']
+    if cleaner == "ImageCleanerSST":
+        image_processor = ImageProcessor(subarray=subarray)
+        image_processor.use_telescope_frame = config["ImageProcessor"][
+            "use_telescope_frame"
+        ]
         if ~image_processor.use_telescope_frame:
             DEFAULT_IMAGE_PARAMETERS_CAMFRAME = deepcopy(ImageParametersContainer())
             DEFAULT_IMAGE_PARAMETERS_CAMFRAME.hillas = CameraHillasParametersContainer()
@@ -369,21 +411,21 @@ def image_cleaner_setup(subarray=None, config=None, ismc=False):
 
         if ismc:
             image_cleaner_sst = ImageCleaner.from_name(
-                    cleaner+'_MC', subarray=subarray, parent=image_processor
-                )
+                cleaner + "_MC", subarray=subarray, parent=image_processor
+            )
             image_processor.clean = image_cleaner_sst
         else:
             image_cleaner_sst = ImageCleaner.from_name(
-                    cleaner, subarray=subarray, parent=image_processor
-                )
+                cleaner, subarray=subarray, parent=image_processor
+            )
             image_processor.clean = image_cleaner_sst
             image_cleaner_sst.average_charge = 0
             image_cleaner_sst.stdev_charge = 0
             image_cleaner_sst.nsb_level = 0
 
-        image_cleaner_sst.config = config['ImageProcessor'][cleaner]
+        image_cleaner_sst.config = config["ImageProcessor"][cleaner]
 
     else:
-        image_processor   = ImageProcessor(subarray=subarray, config=config)
+        image_processor = ImageProcessor(subarray=subarray, config=config)
 
     return image_processor
